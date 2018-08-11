920ec612e84cbe14cfa5fb88a453b7bf6b34f57c
brianriosv <brianriosv@gmail.com>
2018-08-11 12:35:44 -0300

    ex4 and ex5 added

---
+++ Exercise4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            int cantpar = 0;
+            int cantimpar = 0;
+
+            Console.WriteLine("Ingresar diez numeros: ");
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write(". ");
+                num = int.Parse(Console.ReadLine());
+
+                if (num % 2 == 0)
+                    cantpar += 1;
+                else
+                    cantimpar += 1;              
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Cantidad de numeros de pares: " + cantpar);
+            Console.WriteLine("Cantidad de numeros impares: " + cantimpar);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Exercise5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Exercise5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float sum = 0;
+            float cont = 0;
+            float prom;
+            int cantnum;
+            int num;
+
+            Console.Write("Cantidad de numero a ingresar: ");
+            cantnum = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Ingrese los numeros: ");
+            Console.WriteLine();
+
+            for (int i = 0; i < cantnum; i++)
+            {
+                Console.Write(". ");
+                num = int.Parse(Console.ReadLine());
+
+                sum += num;
+                cont++;
+            }
+
+            prom = sum / cont;
+            Console.WriteLine();
+            Console.WriteLine("La sumatoria es: " + sum);
+            Console.WriteLine("El promedio es: " + prom);
+            Console.ReadLine();
+        }
+    }
+}
