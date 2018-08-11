eee23910923f295c6cc04118094e850e4486a156
brianriosv <brianriosv@gmail.com>
2018-08-11 11:01:31 -0300

    add exercises

---
+++ Exercise3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char iter = 's';
+            int factorial = 1; //...
+
+            while (iter == 's') {
+                Console.Write("Ingrese un numero: ");
+                int num = int.Parse(Console.ReadLine());
+
+                if (num < 0)
+                {
+                    Console.WriteLine("El numero es negativo.");
+                }
+                else
+                {
+                    for (int i = 1; i <= num; i++)
+                    {
+                        factorial *= i;
+                    }
+                    Console.Write("El factorial de " + num + " es: " + factorial + "   ");
+                }
+                Console.WriteLine("¿Desea continuar (S/N)?");
+                iter = char.Parse(Console.ReadLine());
+                factorial = 1;
+            }
+            
+        }
+    }
+}
